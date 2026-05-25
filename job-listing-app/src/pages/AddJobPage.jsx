bc3d979cdaae9cbcf92b84d95900a3f90ec3b4f1
Emir Mehinbasic <emir96meh@gmail.com>
2026-05-25 15:18:33 +0200

    Finished Submision form for posting job

--- job-listing-app/src/pages/AddJobPage.jsx
+++ job-listing-app/src/pages/AddJobPage.jsx
@@ -1,26 +1,42 @@
 import React from "react";
-import {useState} from 'react'  
-
-
+import { useState } from "react";
 
 const AddJobPage = () => {
-    const [title, setTitle] = useState('');
-    const [type, setType] = useState('');
-    const [location, setLocation] = useState('');
-    const [description, setDescription] = useState('');
-    const [salary, setSalary] = useState('');
-    const [companyName, setCompanyName] = useState(''); 
-    const [companyDescription, setCompanyDescription] = useState('');
-    const [contactEmail, setContactEmail] = useState('');
-    const [contactPhone, setContactPhone] = useState('');
+  const [title, setTitle] = useState("");
+  const [type, setType] = useState("Full-Time");
+  const [location, setLocation] = useState("");
+  const [description, setDescription] = useState("");
+  const [salary, setSalary] = useState("Under $50K");
+  const [companyName, setCompanyName] = useState("");
+  const [companyDescription, setCompanyDescription] = useState("");
+  const [contactEmail, setContactEmail] = useState("");
+  const [contactPhone, setContactPhone] = useState("");
 
-
+  const submitForm = (e) => {
+    e.preventDefault();
+    const newJob = {
+      title,
+      type,
+      location,
+      description,
+      salary,
+      company: {
+        name: companyName,
+        description: companyDescription,
+        contact: {
+          email: contactEmail,
+          phone: contactPhone,
+        },
+      },
+    };
+    console.log(newJob);
+  };
 
   return (
     <section className="bg-indigo-50">
       <div className="container m-auto max-w-2xl py-24">
         <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
-          <form>
+          <form onSubmit={submitForm}>
             <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
 
             <div className="mb-4">
@@ -156,8 +172,8 @@ const AddJobPage = () => {
                 className="border rounded w-full py-2 px-3"
                 rows="4"
                 placeholder="What does your company do?"
-                value = {companyDescription}
-                onChange = {(e)=> setCompanyDescription(e.target.value)}
+                value={companyDescription}
+                onChange={(e) => setCompanyDescription(e.target.value)}
               ></textarea>
             </div>
 
